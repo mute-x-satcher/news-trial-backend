cd7ce22ad1558d027c1780915102e2b9f6099ec9
Shrirang Mohare <166914392+mute-x-satcher@users.noreply.github.com>
2025-03-12 20:04:03 +0530

    Update connectdb.js

--- backend/db/connectdb.js
+++ backend/db/connectdb.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI;
+// const MONGO_URI = process.env.MONGO_URI;
 console.log("MONGO_URI:", process.env.MONGO_URI);
 const connectDB = async()=>{
 
